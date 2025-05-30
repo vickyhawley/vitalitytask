748f4ebaca5b6f0bc1213cfaa8a7134f0e81cb4b
Victoria Hawley <v.l.hawley@hotmail.com>
2025-05-30 08:44:54 +0100

    aria outer

--- react-accordian-app/src/app/components/accordion/accordion.tsx
+++ react-accordian-app/src/app/components/accordion/accordion.tsx
@@ -15,12 +15,13 @@ export function Accordion({data}:iAccordion){
 
 return(
     
-    <div className={styles['accordion']}>
+    <div role="region" 
+    aria-labelledby="accordion-header" className={styles['accordion']}>
         <Stack className={styles['accordion__top']} direction={'column'} gap={1.2}>
-            <h2 style={{color: '#333333', fontWeight: 600, fontSize: 24, lineHeight: 2, letterSpacing: -1}}>{data.header}</h2>
+            <h2 id="accordion-header" style={{color: '#333333', fontWeight: 600, fontSize: 24, lineHeight: 2, letterSpacing: -1}}>{data.header}</h2>
             <p style={{color: '#333333', fontWeight: 400, fontSize: 16, letterSpacing: 0}}>{data.description}</p>
         </Stack>
-       {data.body?.map((item) =>  <InnerComponent open={open} label={item.label} description={item.description}/>)}
+       {data.body?.map((item, index) =>  <InnerComponent key={`${item.label}-${index}`} open={open} label={item.label} description={item.description}/>)}
     </div>
 )
 }
