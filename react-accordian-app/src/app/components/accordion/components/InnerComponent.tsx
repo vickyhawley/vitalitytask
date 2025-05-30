523eb110b0b8762d383b8c278f44b5707739472a
Victoria Hawley <v.l.hawley@hotmail.com>
2025-05-30 08:42:37 +0100

    aria inner

--- react-accordian-app/src/app/components/accordion/components/InnerComponent.tsx
+++ react-accordian-app/src/app/components/accordion/components/InnerComponent.tsx
@@ -21,10 +21,23 @@ export function InnerComponent({label, description}:iInnerComponent){
 
     }
 
+    // Generate IDs for aria-controls and aria-labelledby to ensure uniqueness if multiple accordions used
+    const buttonId = `accordion-button-${label.replace(/\s+/g, '-').toLowerCase()}`;
+    const panelId = `accordion-panel-${label.replace(/\s+/g, '-').toLowerCase()}`;
+
+
     return(
         <Stack className={styles['accordion__wrapper']}>
             <Stack className={styles['accordion--close']}>
-                <div onClick={() => handleOpen()}>
+                <button id={buttonId}
+                    aria-expanded={open}
+                    aria-controls={panelId} onClick={() => handleOpen()}
+                    style={{
+                            all: 'unset', // remove default button styles, but keep keyboard focus and accessibility
+                            cursor: 'pointer',
+                            width: '100%',
+                        }}
+                    >
                     <Stack direction={'row'} gap={4} alignItems={'center'} justifyContent={'space-between'}>
                         <Stack className={styles['accordion__close--label']} gap={2} >
                             <HappyFace fill={'#e71757'}/>
@@ -32,9 +45,11 @@ export function InnerComponent({label, description}:iInnerComponent){
                         </Stack>
                         {open ? <ChevronUp size={40}/> : <ChevronDown stroke={'#EA194D'} fill={'#F3F6F9'} size={40}/>}
                     </Stack>
-                </div>
+                </button>
             </Stack>
-            {open && <Stack className={styles['accordion--open']}>
+            {open && <Stack id={panelId}
+                    role="region"
+                    aria-labelledby={buttonId} className={styles['accordion--open']}>
                 <p className={styles['accordion__font']}>{description}</p></Stack>}
         </Stack>
     )
